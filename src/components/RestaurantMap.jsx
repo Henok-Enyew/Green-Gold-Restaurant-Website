3c099bd160d2efe12b51047b741606c919a12c74
Henok-Enyew <henokenyew86@gmail.com>
2024-12-06 19:52:36 +0300

    favicons added

--- src/components/RestaurantMap.jsx
+++ src/components/RestaurantMap.jsx
@@ -4,9 +4,23 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "leaflet-routing-machine";
 import "leaflet-control-geocoder";
+import { useMediaQuery } from "react-responsive";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix for missing markers in deployed version
+L.Marker.prototype.options.icon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
 const RestaurantMap = () => {
   const restaurantPosition = [11.612071586784412, 37.427283143631335]; // Restaurant location (latitude, longitude)
+
+  // Detect screen size for mobile
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   const Routing = () => {
     const map = useMap();
@@ -41,27 +55,34 @@ const RestaurantMap = () => {
   };
 
   return (
-    <MapContainer
-      center={restaurantPosition}
-      zoom={15}
-      className="h-96 w-full"
-      //   style={{ height: "500px", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {/* <TileLayer
-        url="https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-        attribution='&copy; <a href="https://www.esri.com/">ESRI</a> contributors'
-      /> */}
-      <Marker position={restaurantPosition} interactive={true}>
-        <Popup>
-          <span> Green Gold Restaurant</span>
-        </Popup>
-      </Marker>
-      <Routing />
-    </MapContainer>
+    <div className="relative">
+      {/* Render info panel only on non-mobile screens */}
+      {!isMobile && (
+        <div className="info-panel absolute top-0 left-0 bg-white p-4 shadow-md">
+          {/* Add start place and destination information here */}
+          <p>Start Place: Auto-detected</p>
+          <p>Destination: Green Gold Restaurant</p>
+        </div>
+      )}
+      <MapContainer
+        center={restaurantPosition}
+        zoom={15}
+        className="h-96 w-full"
+        touchZoom={true} // Enable touch zoom for mobile
+        scrollWheelZoom={true} // Enable scroll zoom
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={restaurantPosition} interactive={true}>
+          <Popup>
+            <span>Green Gold Restaurant</span>
+          </Popup>
+        </Marker>
+        <Routing />
+      </MapContainer>
+    </div>
   );
 };
 
